52fe080b42c3c0104972d22904e4a5245ef96e41
Anjana Baskaran <anjanabaskaran2005@gmail.com>
2025-08-03 21:22:42 +0530

    Added Top doctor section

---
+++ frontend/src/components/TopDoctors.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { doctors } from '../assets/assets_frontend/assets'
+import { useNavigate } from 'react-router-dom'
+
+const TopDoctors = () => {
+
+  const navigate = useNavigate()  
+  return (
+    <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
+        <h1 className='text-3xl font-mediu'>Top Doctor to Book</h1>
+        <p className='text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
+        <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+            {doctors.slice(0,10).map((item,index) => (
+            <div onClick={() => navigate(`/appointment/${item.id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                <img className='bg-blue-50' src={item.image} alt="" />
+                <div className='p-4'>
+                    <div className='flex flex-row items-center gap-2 text-sm text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                    </div>
+                    <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                    <p className='text-gray-600 text-sm'>{item.speciality}</p>
+                </div>
+            </div>    
+            ))}
+        </div>
+        <button className='bg-blue-100 text-gray-600 rounded-full px-12 py-3 mt-10'>more</button>
+    </div>
+  )
+}
+
+export default TopDoctors
